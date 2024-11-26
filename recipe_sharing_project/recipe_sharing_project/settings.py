c255ebe4d952176d1544e1760da9312658b43d18
prathmesh2801 <prathmesh280199@gmail.com>
2024-11-26 16:09:34 +0530

    done vercel settings

--- recipe_sharing_project/recipe_sharing_project/settings.py
+++ recipe_sharing_project/recipe_sharing_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0+e4f%bj0!7e)+z&$)3xpm5*sc1$9_x)4ffnk8cd1jqv&ya^%d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -129,7 +129,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+import os 
 STATIC_URL = 'static/'
+STATICFILES_DIRS=os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
